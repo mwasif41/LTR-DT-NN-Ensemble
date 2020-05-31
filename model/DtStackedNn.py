76d4c1e5549520e3434fd82ffa659e82fc747b14
Muhammad Wasif <Muhammad_Wasif@gap.com>
2020-05-31 10:56:33 +0500

    Added Stacking models

--- model/DtStackedNn.py
+++ model/DtStackedNn.py
@@ -1,6 +1,6 @@
 from model.NeuralNetwork import DeepNeuralNetwork
-import numpy as np
 from constant.Constant import INPUT_DIMS_MQ2008
+from model.DecisionTree import DecisionTree
 
 
 class DtStackedNn:
@@ -9,14 +9,17 @@ class DtStackedNn:
 
     def __init__(self):
         self.nn_model = DeepNeuralNetwork(INPUT_DIMS_MQ2008 + 1)
-        # TODO change this to DT implementation
-        self.dt_model = DeepNeuralNetwork(INPUT_DIMS_MQ2008)
+        self.dt_model = DecisionTree(200)
 
     def fit(self, train_data):
-        # TODO:  This will be done once got decision tree classifier
         self.dt_model.fit(train_data)
-        self.nn_model.fit(train_data)
+        pred = self.dt_model.predict(train_data)
+        stacked_data = train_data
+        stacked_data[len(stacked_data.columns)] = pred
+        self.nn_model.fit(stacked_data)
 
     def predict(self, test_data):
-        # TODO:  This will be done once got decision tree classifier
-        return  np.asarray(self.dt_model.predict(test_data))
+        pred = self.dt_model.predict(test_data)
+        stacked_data = test_data
+        stacked_data[len(stacked_data.columns)] = pred
+        return self.nn_model.predict(stacked_data)
---
+++ runner/DtStackedNnrRunner.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+from util.Utils import read_dataset_as_df
+from constant.Constant import DATASET_MQ2008_PATH
+from constant.Constant import MQ2008_TSV_FILE_NAME
+from model.DtStackedNn import DtStackedNn
+from util.Utils import get_data_params
+from util.Utils import calculate_ndcg
+from util.Utils import calculate_map
+
+
+''''
+Main Running logic for the Deep NN model and Decision tree with ensemble technique
+
+Technique : Stacking (DT -> NN)
+Dataset : MQ2008
+
+'''
+
+print(":: Stacking (DT -> NN) started ::")
+df = read_dataset_as_df(DATASET_MQ2008_PATH + MQ2008_TSV_FILE_NAME)
+# Dividing the data
+train, test = train_test_split(df, test_size=0.7)
+train_x, train_y, train_q = get_data_params(train)
+test_x ,test_y , test_q = get_data_params(test)
+model = DtStackedNn()
+model.fit(train)
+pred = model.predict(test)
+print("predicted value :" , pred)
+ndcg = calculate_ndcg(pred, test_y)
+mAP = calculate_map(pred, test_y)
+
+print('NDCG For Deep NN and DT Stacking (DT -> NN) :', ndcg)
+print('MAP For Deep NN and DT Stacking (DT -> NN) :', mAP)
